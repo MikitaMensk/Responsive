a37d56edb04d49d01b1d5de834ae1db17a3c754b
MikitaMensk <aquarel22@tut.by>
2017-09-26 12:55:11 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,96 @@
+var app = angular.module("testTask", []);
+
+app.controller('productsCtrl', ['$scope', function ($scope) {
+	
+	$scope.data = {
+		"items": [
+		{
+			"type" : "Child",
+			"img" : "item1",
+			"price" : "3.20",
+			"name" : "T-SHIRT",
+			"options": ["Size", "S", "M", "L", "XL"]
+		}
+		,{
+			"type" : "Child",
+			"img" : "item2",
+			"price" : "13.30",
+			"name" : "Pants FORCLAZ",
+			"options": ["Size", "M", "XL"]
+		}
+		,{
+			"type" : "Men",
+			"img" : "item1",
+			"price" : "5.00",
+			"name" : "T-SHIRT",
+			"options": ["Size", "XL"]
+		}
+		,{
+			"type" : "Women",
+			"img" : "item2",
+			"price" : "3.21",
+			"name" : "T-SHIRT",
+			"options":  ["Size", "S", "M", "L", "XL"]
+		}
+		,{
+			"type" : "Women",
+			"img" : "item3",
+			"price" : "31",
+			"name" : "backpack",
+			"options": ["color", "red", "blue", "black"]
+		}
+		, {
+			"type" : "Other",
+			"img" : "item3",
+			"price" : "0",
+			"name" : "car",
+			"options": ["color", "red", "blue", "black"]
+		}
+  	]
+	};
+
+	$scope.showCategory = {
+			"Men" : true,
+			"Women" : true,
+			"Child": false
+	};
+	
+	$scope.changeFilter  = function (obj) {
+        $scope.showCategory.Other = true;
+        $scope.showCategory.Men = false;
+        $scope.showCategory.Women = false;
+        $scope.showCategory.Child = false;
+		return !$scope.showCategory[obj];
+	}
+
+	
+}]);
+
+
+app.controller('menuCtrl', ['$scope', function ($scope) {
+	
+	$scope.data = {
+		"footerMenu" : [
+			{
+				"name": "Credits",
+				"link": "https://softswiss.com/",
+				"title": "credits"
+			},
+			{
+				"name": "Privacy",
+				"link": "https://softswiss.com/",
+				"title": "privacy"
+			},
+			{
+				"name": "About",
+				"link": "https://www.softswiss.com/about-us/",
+				"title": "about us"
+			},
+			{
+				"name": "Contact",
+				"link": "https://www.softswiss.com/contact-us/",
+				"title": "contact us"
+			}
+		]};
+
+}]);
